258f321f4fbb1f7ebcb72f8f34365556a4ba8582
Alvaro Perez <>
2023-02-16 16:40:04 -0400

    Add folder Library App

---
+++ LibraryApp/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraryApp
+{
+    public class Book
+    {
+        public string Author { get; set;}
+        public string Title { get; set; }
+        public int Pages { get; set; }
+        public string Editorial { get; set; }
+        
+        public Book(string author, string title, int pages, string editorial)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Pages = pages;
+            this.Editorial = editorial;
+        }
+
+    }
+}
---
+++ LibraryApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LibraryApp
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonStringPath = "C:\\Users\\ASUS\\source\\repos\\LibraryApp\\ShelfContainer\\Quijote.json";
+            var jsonContent = File.ReadAllText(jsonStringPath);
+            var quijote = JsonConvert.DeserializeObject<Book>(jsonContent);
+
+            var bible = new Book("Many Authors", "La Biblia", 1800, "Vaticano");
+
+            var bibleSerialized = JsonConvert.SerializeObject(bible);
+            File.WriteAllText("C:\\Users\\ASUS\\source\\repos\\LibraryApp\\ShelfContainer\\Bible.json", bibleSerialized);
+        }
+    }
+}
